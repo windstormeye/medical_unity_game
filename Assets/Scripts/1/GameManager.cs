fde80962da2575c4de3433a8ec653d3318dbc3f7
pjhubs <877302410@qq.com>
2020-12-18 23:22:07 +0800

    【feature】完成 第三幕核心逻辑

---
+++ Assets/Scripts/1/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private OldLadyController oldLady;
+
+    private void Start()
+    {
+        StartCoroutine(DelayShowEatPillPanel());
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        oldLady = FindObjectOfType<OldLadyController>();
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator DelayShowEatPillPanel()
+    {
+        // NOTE: 延迟 5s 出慈丹
+        yield return new WaitForSeconds(5);
+        UIManager.instance.ShowEatPillPanel();
+
+    }
+}
