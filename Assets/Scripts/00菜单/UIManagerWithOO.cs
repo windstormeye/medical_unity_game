3628ad90ba77329b9da6060574a9a48792c07c2a
pjhubs <877302410@qq.com>
2021-01-04 23:19:20 +0800

    【feature】增加 菜单入坑

---
+++ Assets/Scripts/00菜单/UIManagerWithOO.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerWithOO : MonoBehaviour
+{
+    // 饮食
+    public GameObject diet;
+    public GameObject pill;
+    public GameObject sport;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoDietScene()
+    {
+
+    }
+
+    public void GotoPillScene()
+    {
+
+    }
+
+    public void GotoSportScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/01老奶奶/5/FiveGameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FiveGameManager : MonoBehaviour
+{
+    public static FiveGameManager instance;
+    public GameObject menuPanel;
+    public GameObject overPanel;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayShowMenuPanel());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // NOTE: 休息
+    public void Break()
+    {
+        menuPanel.SetActive(false);
+        overPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 4);
+    }
+
+    private IEnumerator DelayShowMenuPanel()
+    {
+        // NOTE: 延迟 5s 出菜单
+        yield return new WaitForSeconds(5);
+        menuPanel.SetActive(true);
+    }
+}
