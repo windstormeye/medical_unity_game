783d96f0485bc61635b67c5a3b66438eb11d4db8
pjhubs <877302410@qq.com>
2021-02-22 09:47:25 +0800

    【feature】搭建 饮食案例一 UI 框架

---
+++ Assets/Scripts/饮食/UIManagerWithFoodFirst.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManagerWithFoodFirst : MonoBehaviour
+{
+
+    public GameObject FirstPanel;
+    public GameObject SecondPanel;
+    public GameObject ThirdPanel;
+    public GameObject FourPanel;
+    public GameObject FivePanel;
+    public GameObject OverPanel;
+    public GameObject RestartPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showFirstPanel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showFirstPanel()
+    {
+        FirstPanel.SetActive(true);
+    }
+}
